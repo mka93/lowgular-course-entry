325de31548aca924fa9c511ceaa65843eb89962c
mka93 <karuska@onet.eu>
2022-11-14 23:10:09 +0100

    update

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,10 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
+import * as url from "url";
 
 @Component({
   selector: 'employee-list',
@@ -13,5 +11,13 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  get "https://eqsfaxnghe.cfolks.pl/assets/data/employees.json"() {
+    return this["_https://eqsfaxnghe.cfolks.pl/assets/data/employees.json"];
+  }
+
+  set "https://eqsfaxnghe.cfolks.pl/assets/data/employees.json"(value) {
+    this["_https://eqsfaxnghe.cfolks.pl/assets/data/employees.json"] = value;
+  }
+  constructor(private _client: HttpClient) {}
+  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>(url: 'assets/data/employees.json');
 }
